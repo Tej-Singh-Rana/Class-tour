603a13f9c5761976d53d2b182281a45ec58c59b5
rana <root@rana.com>
2019-11-29 11:08:04 -0500

    base

---
+++ practical2.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+#taking two int value from user.
+
+print("Enter the two integer value : ")
+value,volt=int(input()),int(input())
+
+if value>volt:
+    print("It is a greater from second value")
+elif value == volt:
+    print("Both are equal")
+else:
+    print("It is a greater from first value")
+
+
+
+
+
+
+
+
+
+
+
